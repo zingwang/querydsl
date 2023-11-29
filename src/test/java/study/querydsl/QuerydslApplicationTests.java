e55a37b1b6b31c996e46b819810a8a1738d646f2
DESKTOP-95CBTFN\Admin <zingwang06@naver.com>
2023-11-29 19:23:37 +0900

    Querydsl 설정

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,40 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Transactional
 @SpringBootTest
 class QuerydslApplicationTests {
 
+
+	@Autowired
+	EntityManager em;
+
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+
+		QHello qHello = new QHello("h");
+
+		Hello  result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
